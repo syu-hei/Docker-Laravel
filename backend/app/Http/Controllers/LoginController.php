015597b02c5c9247e56bf4bd1b74b9da82e5e5d5
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-11-28 05:26:02 +0900

    Update LoginController.php

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -16,22 +16,19 @@ class LoginController extends Controller
 		$client_master_version = $request->client_master_version;
 		$user_id = $request->user_id;
 
-		//マスターデータチェック
 		if (!MasterDataService::CheckMasterDataVersion($client_master_version)) {
 			return config('error.ERROR_MASTER_DATA_UPDATE');
 		}
 
 		//user_profileテーブルのレコードを取得
 		$user_profile = UserProfile::where('user_id', $user_id)->first();
-		//レコード存在チェック
+
 		if (!$user_profile) {
 			return config('error.ERROR_INVALID_DATA');
 		}
 
-		//ログインボーナステーブルのレコードを取得
 		$user_login = UserLogin::where('user_id', $user_id)->first();
 		if (!$user_login) {
-			//初期値設定
 			$user_login = new UserLogin;
 			$user_login->user_id = $user_id;
 			$user_login->login_day = 0;
@@ -39,7 +36,6 @@ class LoginController extends Controller
 			$user_login->last_login_at = $last_login_at;
 		}
 
-		//日付の比較
 		$today = date('Y-m-d');
 		$last_login_day = date('Y-m-d', strtotime($user_login->last_login_at));
 
@@ -51,20 +47,16 @@ class LoginController extends Controller
 
 
 			if (!is_null($master_login_item)) {
-				//プレゼント作成
 				$user_present->user_id = $user_id;
 				$user_present->item_type = $master_login_item->item_type;
 				$user_present->item_count = $master_login_item->item_count;
 				$user_present->description = 'Loginbonus';
-				//30日後まで受け取りOK
 				$user_present->limited_at = date('Y-m-d', (time() + (60 * 60 * 24 * 30)));
 			}
 		}
 
-		//ログイン時刻の更新
 		$user_login->last_login_at = date("Y-m-d H:i:s");
 
-		//データの書き込み
 		try {
 			if (isset($user_present->user_id)) {
 				$user_present->save();
@@ -73,10 +65,9 @@ class LoginController extends Controller
 		} catch (\PDOException $e) {
 			return config('error.ERROR_DB_UPDATE');
 		}
-		//user_presentテーブルからレコード取得
+
 		$user_present_list = UserPresent::where('user_id', $user_id)->get();
 
-		//クライアントへのレスポンス
 		$response = array(
 			"user_login" => $user_login,
 			"user_present" => $user_present_list,
